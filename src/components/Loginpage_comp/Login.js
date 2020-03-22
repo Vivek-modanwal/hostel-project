b67cab7e787e019440ea036a8eff5b39bf89391c
Vivek modanwal <modanwalvivek20@gmail.com>
2020-03-22 12:02:46 +0530

    updated the login component and approuter component

--- src/components/Loginpage_comp/Login.js
+++ src/components/Loginpage_comp/Login.js
@@ -1,78 +1,102 @@
 import React from "react";
 import validator from "validator";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { Link, Redirect } from "react-router-dom";
 
 class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      passwordError: ""
-    };
-  }
-  Submitted = e => {
-    e.preventDefault();
-    const password = e.target.password.value;
-    const username = e.target.username.value.trim();
-    e.target.password.value = "";
-    e.target.username.value = "";
-    console.log(password);
-    const re = /^[A-Za-z0-9]{8,15}$/;
-
-    this.setState(() => ({
-      passwordError: re.test(password) ? "" : "please enter valid password"
-    }));
-
-    const credential = {};
-    credential.password = password;
-    if (validator.isEmail(username)) {
-      credential.email = username;
-    } else {
-      credential.userid = username;
+    constructor(props) {
+        super(props);
+        this.state = {
+            passwordError: false,
+            invalidCredential: false,
+            redirect: false
+        };
     }
-    console.log(credential);
-    fetch("http://localhost:3000/login", {
-      method: "POST",
-      body: JSON.stringify(credential),
-      headers: {
-        "Content-type": "application/ json; charset = UTF - 8"
-      }
-    })
-      .then(res => {
-        res.json();
-      })
-      .then(data => console.log(data))
-      .catch(e => "please enter valid credentials");
-  };
-  render() {
-    return (
-      <div className="temp">
-        <form onSubmit={this.Submitted}>
-          <p>Enter username</p>
-          <p>
-            <input type="text" name="username" required={true} />
-          </p>
-          <p>Enter password</p>
-          <p>
-            <input type="password" name="password" required={true} />
-          </p>
-          {this.state.passwordError && <p>{this.state.passwordError}</p>}
+    Submitted = async e => {
+        e.preventDefault();
+        const password = e.target.password.value;
+        const username = e.target.username.value.trim();
+        e.target.password.value = "";
+        e.target.username.value = "";
+        const re = /^[A-Za-z0-9]{8,15}$/;
+        try {
+            this.setState(() => ({
+                passwordError: !re.test(password),
+                invalidCredential: false
+            }));
+            if (!re.test(password)) throw new Error("invalid password");
+            const credential = {};
+            credential.password = password;
+            if (validator.isEmail(username)) {
+                credential.email = username;
+            } else {
+                credential.userid = username;
+            }
+            const data = await axios.post(
+                "http://localhost:5000/login",
+                credential
+            );
+            this.setState(() => ({
+                invalidCredential: false
+            }));
+            this.props.authenticated(data.data);
+            this.setRedirect();
+        } catch (e) {
+            const error = e.response;
+            if (error && error.status >= 400 && error.status < 500)
+                this.setState(() => ({
+                    invalidCredential: true
+                }));
+        }
+    };
+    setRedirect = () => {
+        this.setState({
+            redirect: true
+        });
+    };
+    renderRedirect = () => {
+        if (this.state.redirect) {
+            return <Redirect to="/admin" />;
+        }
+    };
+    render() {
+        return (
+            <div className="temp">
+                {this.renderRedirect()}
+                <form onSubmit={this.Submitted}>
+                    {this.state.invalidCredential && (
+                        <p>username or password is incorrect</p>
+                    )}
+                    <p>Enter username</p>
+                    <p>
+                        <input type="text" name="username" required={true} />
+                    </p>
+                    <p>Enter password</p>
+                    <p>
+                        <input
+                            type="password"
+                            name="password"
+                            required={true}
+                        />
+                    </p>
+                    {this.state.passwordError && <p>password is invalid</p>}
 
-          <input type="submit" value="Submit" />
-          <div className="separate.signup_forget">
-            <div>
-              <p>Don't have an Account ?</p>
-              <p>
-                <Link to="/signup">Sign up</Link>
-              </p>
+                    <input type="submit" value="Submit" />
+                    <div className="separate.signup_forget">
+                        <div>
+                            <p>Don't have an Account ?</p>
+                            <p>
+                                <Link to="/signup">Sign up</Link>
+                            </p>
+                        </div>
+                        <p>
+                            <Link to="/pass_reset">Forget Password</Link>
+                        </p>
+                    </div>
+                </form>
             </div>
-            <p>
-              <Link to="/pass_reset">Forget Password</Link>
-            </p>
-          </div>
-        </form>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Login;
